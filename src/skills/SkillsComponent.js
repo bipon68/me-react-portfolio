3d1d09ddf1dde3bf65eac49a17f6c742bc8d99f7
bipon68 <bipon68@users.noreply.github.com>
2020-07-02 15:11:31 +0600

    iterate get data from json file

--- src/skills/SkillsComponent.js
+++ src/skills/SkillsComponent.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import SkillsData from '../DemoData/Skills/Skills';
 
 export default class SkillsComponent extends Component {
     render() {
         return (
             <>
                <section id="skills">
-    	<h1>Skills</h1>
+    	<h1>{SkillsData.skills.title}</h1>
         <dl>
-        	<dt>Design</dt>
-            <dd title="Design of websites on screen, tablets and mobiles">Web Design</dd>
+        	<dt>{SkillsData.skills.design.title}</dt>
+            {
+              SkillsData.skills.design.list.map((item, i) => 
+                <dd title={item.tooltip} key={i}>{item.title}</dd>
+              )  
+            }
+            {/* <dd title="Design of websites on screen, tablets and mobiles">Web Design</dd>
             <dd title="Design of interfaces for apps and web based systems">User Interface Design</dd>
             <dd title="Sketching and wireframing using Balsamiq Mockup">Wireframing / Mockups</dd>
-            <dd title="Turning mockups into comps">Photoshop</dd>
+            <dd title="Turning mockups into comps">Photoshop</dd> */}
+
+            <dt>{SkillsData.skills.frontEnd.title}</dt>
+            {
+              SkillsData.skills.frontEnd.list.map((item, i) => 
+                <dd title={item.tooltip} key={i}>{item.title}</dd>
+              ) 
+            }
             
-            <dt>Front-End Development</dt>
+            {/* <dt>Front-End Development</dt>
             <dd title="Hyper Text Markup Language">HTML5</dd>
             <dd title="Cascading Style Sheets">CSS3</dd>
             <dd title="Front-end programming language">JavaScript</dd>
             <dd title="Javascript Library">jQuery</dd>
             <dd title="Ensuring website/app has usable experience on all platforms">Cross-browser Compatibility</dd>
-            <dd title="Coding markup in an accessible, usable and semantic way, using best practices and avoiding proprietary rules.">Web Standards</dd>
+            <dd title="Coding markup in an accessible, usable and semantic way, using best practices and avoiding proprietary rules.">Web Standards</dd> */}
             
             <dt>Back-end / Integration</dt>
             <dd title="PHP: Hypertext Preprocessor">PHP</dd>
--- src/summary/SummaryComponent.js
+++ src/summary/SummaryComponent.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import summartData from '../DemoData/Summary/Summary';
 
 export default class SummaryComponent extends Component {
     render() {
         return (
             <>
                 <section id="summary">
-                    <h1>Summary</h1>
-                    <p>Web Designer and Front-end Developer since 2013. Worked for agencies and as a freelancer for companies in Bangladesh, in
-                    Asia.</p>
-                    <p>Focus on designing user interfaces for websites and developing semantic, responsive and reusable code aiming for 
-                    best performance on browsers and devices.</p>
-                    <p>A programmer by education (graduated in Computer Science) and (interface) designer by heart.
-                    Also a blogger at jpedroribeiro.com, where several web related topics are written, from book reviews to how-to
-                    guides on development and design.</p>    	
+                    <h1>{summartData.summaryTitle}</h1>
+                    <p>{summartData.initialTitle}</p>
+                    <p>{summartData.focusTitle}</p>
+                    <p>{summartData.educationTitle}</p>    	
                 </section>
             </>
         )
